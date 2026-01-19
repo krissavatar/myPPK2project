d46796a8e1b04430d12689c4ab62924da852706d
krissavatar <chrisavatar56@gmail.com>
2026-01-19 11:54:22 +0100

    Add files via upload

---
+++ blinktest2/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+#define LED 2
+
+void setup() {
+  // Set pin mode
+  pinMode(LED,OUTPUT);
+}
+
+void loop() {
+  delay(1000);
+// you can set the delay time by adjusting the parameter of delay();
+  digitalWrite(LED,HIGH);
+  delay(1000);
+  digitalWrite(LED,LOW);
+}
